625b41f7209ed58af6148f831a990a5e91138280
Ella <Ella>
2021-03-18 11:39:07 +0100

    Done!

---
+++ js/index.js
@@ -0,0 +1,12 @@
+function addOperation(){
+    document.answer.result.value += '+' .value = "+";
+}
+function subOperation(){
+    document.answer.result.value += '-'. value ="-"
+}
+function mulOperation(){
+    document.answer.result.value += '*'. value ="*"
+}
+function divOperation(){
+    document.answer.result.value += '/'. value ="/"
+}
